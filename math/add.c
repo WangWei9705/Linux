121c40e5e1e6aa9cb3ff5bc03fc30bf724a481b6
nubiaX <872408568@qq.com>
2019-04-29 22:18:05 +0800

    动态库、静态库的使用

---
+++ math/add.c
@@ -0,0 +1,14 @@
+/**********************************************************
+ * Author        : WangWei
+ * Email         : 872408568@qq.com
+ * Last modified : 2019-04-29 20:56:49
+ * Filename      : add.c
+ * Description   : 
+ * *******************************************************/
+
+#include "add.h"
+
+int add(int a, int b) {
+    return a + b;
+
+}
---
+++ math/div.h
@@ -0,0 +1,6 @@
+#ifndef _DIV_H
+
+#define _DIV_H
+int div(int a, int b);
+
+#endif

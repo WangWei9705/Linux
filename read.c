89e2efae48ed05f8d31331fde9fe37faf2c38967
nubiaX <872408568@qq.com>
2019-06-22 22:45:27 +0800

    系统IO接口——读

--- read.c
+++ read.c
@@ -1,34 +1,39 @@
 /**********************************************************
  * Author        : WangWei
  * Email         : 872408568@qq.com
- * Last modified : 2019-06-22 21:41:39
+ * Last modified : 2019-06-22 22:19:56
  * Filename      : read.c
- * Description   : 标准IO接口——读
+ * Description   : 系统IO接口——读
  * *******************************************************/
 #include <stdio.h>
-#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include <string.h>
 
 int main()
 {
-    FILE* fp = fopen("Linux.txtx", "r");
-    if(!fp) {
-        printf("fopen error \n");
+    int fd = open("test.txt", O_RDONLY);
+    if(fd < 0) {
+        perror("open error");
+        return 1;
+        
     }
+
+    const char *msg = "晚上好！\n";
 
     char buf[1024];
 
-    const char *msg = "我爱学习！\n";
-
     while(1) {
-        ssize_t s = fread(buf, 1, strlen(msg), fp);
+        ssize_t s = read(fd, buf, strlen(msg));
         if(s > 0) {
-            buf[s] = 0;
             printf("%s", buf);
+        } else {
             break;
         }
     }
-    fclose(fp);
+    close(fd);
     printf("Hello world\n");
     return 0;
 }
